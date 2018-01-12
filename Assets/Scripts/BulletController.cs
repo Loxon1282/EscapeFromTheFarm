0ef6349ad212105002c78e083e74b15a509a07c2
Loxon1282 <loxon1282@gmail.com>
2018-01-12 21:46:57 +0100

    Kot_1
    Camera, TestScene, BulletController,Test assets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float force = 5.0f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.isKinematic = true;
+		StartCoroutine ("TestfireDelay");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Fire(){
+
+		rb.AddForce (new Vector2 (1, 1) * force);
+
+	}
+
+	IEnumerator TestfireDelay(){
+		yield return new WaitForSeconds (2);
+		rb.isKinematic = false;
+		Fire ();
+
+	}
+}
