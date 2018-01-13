50e27333a4484e146381246ff84dc99b632191a4
Loxon1282 <loxon1282@gmail.com>
2018-01-13 13:16:10 +0100

    SomeBonuses

---
+++ Assets/Scripts/BonusController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusController : MonoBehaviour {
+
+	Rigidbody rb;
+
+	public float forageForce = 5.0f;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.tag == "Forage") {
+			rb.AddForce (new Vector3 (1, 0.8f,0) * forageForce);
+		}
+
+
+	}
+}
