e50a26f04ebd7dfcaaf88f54cd6d022d2a0ed286
Jakub <jakub.kicmal@gmail.com>
2018-01-13 12:23:26 +0100

    AnimacjeDrzew

---
+++ Assets/Scripts/Trees_Angle_Randomizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trees_Angle_Randomizer : MonoBehaviour {
+
+	public bool freezeX, freezeY, freezeZ;
+
+	void Start () {
+		float rotX, rotY, rotZ;
+		if (freezeX) {
+			rotX = gameObject.transform.rotation.eulerAngles.x;
+		} else {
+			rotX = Random.Range (-2.0f, 2.0f) + gameObject.transform.rotation.eulerAngles.x;	
+		}
+		if (freezeY) {
+			rotY = gameObject.transform.rotation.eulerAngles.y;
+		} else {
+			rotY = Random.Range (0, 180) + gameObject.transform.rotation.eulerAngles.y;	
+		}
+		if (freezeZ) {
+			rotZ = gameObject.transform.rotation.eulerAngles.z;
+		} else {
+			rotZ = Random.Range (-2.0f, 2.0f) + gameObject.transform.rotation.eulerAngles.z;	
+		}
+		gameObject.transform.rotation = Quaternion.Euler( new Vector3(rotX, rotY, rotZ));
+	}
+
+}
