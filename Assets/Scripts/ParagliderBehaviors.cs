db8569b2f26d9f1b4abb3eb5493192386926e20b
Loxon1282 <loxon1282@gmail.com>
2018-01-13 20:39:29 +0100

    asdasasdasdas

---
+++ Assets/Scripts/ParagliderBehaviors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParagliderBehaviors : MonoBehaviour {
+
+	Rigidbody rb;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	void Update () {
+		Paragliding ();
+	}
+
+	void Paragliding(){
+		if (rb.velocity.x >0) {
+			if (transform.rotation.x < 0.5f&& transform.rotation.x > -0.5f) {
+
+
+
+			} else
+				print ("Not OK");
+
+		}
+	}
+}
