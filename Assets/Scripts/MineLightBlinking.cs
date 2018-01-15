87ce184c44ad9dee3d75cf5c5b4621ca169081db
Jakub <jakub.kicmal@gmail.com>
2018-01-15 21:43:32 +0100

    Miny

---
+++ Assets/Scripts/MineLightBlinking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineLightBlinking : MonoBehaviour {
+
+	private Light light;
+	private float range;
+
+	void Start () {
+		light = gameObject.GetComponent<Light> ();
+		range = light.range;
+		InvokeRepeating ("Blink", Random.value, 0.7f);
+	}
+
+	void Blink() {
+		if (light.range != 0) {
+			light.range = 0;
+		} else {
+			light.range = range;
+		}
+	}
+}
--- Assets/Scripts/TreesAngleRandomizer.cs
+++ Assets/Scripts/TreesAngleRandomizer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trees_Angle_Randomizer : MonoBehaviour {
+public class TreesAngleRandomizer : MonoBehaviour {
 
 	public bool freezeX, freezeY, freezeZ;
 
