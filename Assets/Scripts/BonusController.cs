0ec6986f3d3ed4cf4e88db5d43c1b2c4e74107ad
Loxon1282 <loxon1282@gmail.com>
2018-01-16 00:26:04 +0100

    Today is my day

--- Assets/Scripts/BonusController.cs
+++ Assets/Scripts/BonusController.cs
@@ -7,9 +7,17 @@ public class BonusController : MonoBehaviour {
 	Rigidbody rb;
 
 	public float forageForce = 5.0f;
+	bool baloon = false;
+	GameObject baloonObj;
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 
+	}
+	void Update(){
+		if (baloon) {
+			BallonBonus ();
+
+		}
 	}
 
 
@@ -24,6 +32,34 @@ public class BonusController : MonoBehaviour {
 			//Tutaj funkcja endu poziomu
 		}
 
+		else if (other.tag == "Baloon") {
+			
+			baloon = true;
+			StartCoroutine ("BaloonTime");
+
+			baloonObj = other.gameObject;
+
+		}
+
+
+
 
 	}
+	IEnumerator BaloonTime(){
+		yield return new WaitForSeconds (2);
+		baloon = false;
+		Destroy (baloonObj);
+	}
+
+	void BallonBonus(){
+		baloonObj.transform.position = new Vector3(transform.position.x,transform.position.y+baloonObj.transform.localScale.y,transform.position.z);
+		rb.AddForce (new Vector3 (0, 5.0f, 0));
+	}
+
+	public void FartPower(float AmountOfGas){
+		
+
+	}
+
+
 }
