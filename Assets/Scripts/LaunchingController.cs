d9b746d4434d6eb2f6c3764c6d148f75e363a89b
Jakub <jakub.kicmal@gmail.com>
2018-02-15 20:27:26 +0100

    Katapulta Wystrzeliwanie Wersja OMEGA

--- Assets/Scripts/LaunchingController.cs
+++ Assets/Scripts/LaunchingController.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class LaunchingController : MonoBehaviour {
 
+	/* TUTORIAL
+	 * F1 to ready the spoon for launch
+	 * F2 to launch the player 
+	 * F3 to set spoon to a middle angle
+	 * F4 to reset players position
+	 * WARNING:
+	 * Before reseting position use F1 or F3 to fix spoon angle then F4
+	*/
+
 	public GameObject spoon;
-	public GameObject animalSpot;
-	public GameObject worldTransform;
+	public GameObject animalSpot; // gameobject fixed to spoon position
+	public GameObject worldTransform; // object that is highest in hierarhy
+	public GameObject loader; // crank
+	public GameObject player; // animal
+
 	public Vector3 launchingVelocity;
 	public Vector3 launchingTorque;
 
-	public GameObject player;
-	public Rigidbody playerRb;
+	private Rigidbody playerRb;
 
-	// Lower: 345 = -15(in editor), Uppder: 270 = -90(in editor)
-	public float upperBound;
-	public float lowerBound;
-	public float launchingBound;
-	public float centerAngle;
+	private Quaternion playerAngle; // starting player position
 
-	public float spoonAngle;
-	public float destinationAngle;
+	private float upperBound; // 270, -90 in editor = after launch
+	private float lowerBound; // 345, -15 in editor = ready to launch
+
+	private float launchingBound; // around 280/290
+	private float centerAngle; // first position, spoon in the middle
+
+	private float spoonAngle; // dynamic angle
+	private float loaderAngle; // to rotate crank
+	private float destinationAngle; // where spoon is heading
 	public float spoonSpeed;
+	public int loaderMultiplier;
 
-	public float fastSpeed;
-	public float slowSpeed;
+	public float fastSpeed; // lauching spoon
+	public float slowSpeed; // loading spoon
 
-	public bool canMove;
-	public bool canLaunch;
-	public bool movingUpwards;
+	private bool canMove;
+	private bool canLaunch;
+	private bool movingUpwards; // true = moving towards 270, false = moving towards 345
 
 	void Awake() {
 		canMove = false;
-		canLaunch = false;
+		canLaunch = true;
 
 		lowerBound = 345;
 		upperBound = 270;
@@ -44,9 +59,11 @@ public class LaunchingController : MonoBehaviour {
 
 	void Start () {
 		spoon.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, centerAngle));
-		spoonAngle = spoon.transform.rotation.eulerAngles.z;
 
-		TestingSpoonMovement ();
+		playerAngle = player.transform.localRotation;
+
+		loaderAngle = loader.transform.rotation.eulerAngles.x;
+		spoonAngle = spoon.transform.rotation.eulerAngles.z;
 	}
 	
 
@@ -59,13 +76,11 @@ public class LaunchingController : MonoBehaviour {
 			ResetSpoonPosition ();
 		} else if (Input.GetKeyDown (KeyCode.F4)) {
 			ResetPlayer ();
-		} else if (Input.GetKeyDown (KeyCode.F5)) {
-			LaunchPlayer ();
 		}
-		Debug.Log (spoonAngle);
 	}
 
 	void FixedUpdate() {
+		
 		if (canMove) {
 
 			if (canLaunch && spoonAngle <= launchingBound) {
@@ -81,32 +96,39 @@ public class LaunchingController : MonoBehaviour {
 			}
 
 			if (movingUpwards) {
+				loaderAngle -= Time.deltaTime * spoonSpeed * loaderMultiplier;
 				spoonAngle -= Time.deltaTime * spoonSpeed;
 			} else {
+				loaderAngle += Time.deltaTime * spoonSpeed * loaderMultiplier;
 				spoonAngle += Time.deltaTime * spoonSpeed;
 			}
-
+	
+			
 			if (movingUpwards && canLaunch && launchingBound >= spoonAngle) {
 				canLaunch = false;
 				LaunchPlayer ();
 			}
 
 			spoon.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, spoonAngle));
+			loader.transform.rotation = Quaternion.Euler (new Vector3 (loaderAngle , 270, 270));
+
 		}
 	}
 
+
+	// Resets player position after launch
 	void ResetPlayer() {
 		canLaunch = true;
 
 		player.transform.SetParent (animalSpot.transform);
 		player.transform.localPosition = new Vector3 (0, 0, 0);
 
-
 		playerRb.isKinematic = true;
 
-
+		player.transform.localRotation = playerAngle;
 	}
 
+	// Launches player :3
 	void LaunchPlayer() {
 		player.transform.SetParent (worldTransform.transform);
 
@@ -115,6 +137,7 @@ public class LaunchingController : MonoBehaviour {
 		playerRb.AddTorque(launchingTorque);
 	}
 
+	// Sets spoon to lowest position (370)
 	void SetSpoonToLounch() {
 		canMove = true;
 		movingUpwards = false;
@@ -123,6 +146,7 @@ public class LaunchingController : MonoBehaviour {
 		spoonSpeed = slowSpeed;
 	}
 
+	// Sets spoon to highets position (260)
 	void FireSpoon() {
 		canMove = true;
 		movingUpwards = true;
@@ -131,6 +155,7 @@ public class LaunchingController : MonoBehaviour {
 		spoonSpeed = fastSpeed;
 	}
 
+	// Sets spoon to a middle position
 	void ResetSpoonPosition() {
 		canMove = true;
 		destinationAngle = centerAngle;
@@ -138,9 +163,4 @@ public class LaunchingController : MonoBehaviour {
 
 		spoonSpeed = slowSpeed;
 	}
-
-	void TestingSpoonMovement() {
-		
-	}
-
 }
