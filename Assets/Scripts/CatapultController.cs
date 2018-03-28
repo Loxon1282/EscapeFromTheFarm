293e1d0bdc6ba450797a3f32574491788a50a2f4
PanWilson <PanWilsonn@wp.pl>
2018-03-28 18:02:14 +0200

    launcher, spin controller to launcher

---
+++ Assets/Scripts/CatapultController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultController : MonoBehaviour
+{
+
+
+    [SerializeField]
+    RectTransform wheel;
+
+    [SerializeField]
+    float maxDist;          //max distance from wheel center to track touch position
+
+    [SerializeField]
+    float minDist;          //min distance from wheel center to track touch position
+
+    [SerializeField]
+    float maxSpins;
+
+    [SerializeField]
+    float lounchTime;
+
+    [SerializeField]
+    float moveMargin;
+
+    float fingerRotation;
+    float fingerPrevRotation;
+    float fingerDeltaRotation;
+
+    float sum;
+    float maxSum;
+
+    float timer;
+    bool fingerIn = false;
+    bool timerActive = false;
+
+    Launcher launcher;
+
+    // Use this for initialization
+    void Start()
+    {
+        sum = 0;
+        maxSum = maxSpins * Mathf.PI * 2;
+        launcher = GetComponent<Launcher>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timer < lounchTime && Mathf.Abs(sum) < maxSum) SpinLogic();
+        else
+        {
+            Done();
+            Reset();
+        }
+
+        if (timerActive) timer += Time.deltaTime;
+
+    }
+
+
+    void SpinLogic()
+    {
+        if (Input.touchCount >= 1)
+        {
+
+            float touchDist = Vector2.Distance(Input.GetTouch(0).position, wheel.position);
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)                                                //Finger touch
+            {
+
+                if (touchDist >= minDist && touchDist <= maxDist)
+                {
+                    fingerPrevRotation = GetLookAtRotation(wheel.position, Input.GetTouch(0).position);
+                    fingerIn = true;
+                }
+
+            }
+
+
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)                                                //Finger moving
+            {
+
+                if (touchDist >= minDist && touchDist <= maxDist)
+                {
+                    if (fingerIn)
+                    {
+                        fingerRotation = GetLookAtRotation(wheel.position, Input.GetTouch(0).position);
+                        fingerDeltaRotation = fingerRotation - fingerPrevRotation;
+                        fingerPrevRotation = fingerRotation;
+
+                        if (fingerDeltaRotation > 5) fingerDeltaRotation -= Mathf.PI *2;
+                        if (fingerDeltaRotation < -5) fingerDeltaRotation += Mathf.PI * 2;
+
+                        sum += fingerDeltaRotation;
+
+                        SetState();
+
+                        if (!timerActive && Mathf.Abs(sum) >= moveMargin) timerActive = true;
+                    }
+                    else
+                    {
+                        fingerRotation = GetLookAtRotation(wheel.position, Input.GetTouch(0).position);
+                        fingerPrevRotation = fingerRotation;
+                        fingerIn = true;
+                    }
+                }
+                else
+                {
+                    fingerIn = false;
+                }
+
+            }
+
+        }
+    }
+
+    private void Reset()
+    {
+        sum = 0;
+        timer = 0;
+        fingerIn = false;
+        timerActive = false;
+    }
+
+    float GetLookAtRotation(Vector2 from, Vector2 to)
+    {
+        Vector2 hlp = to - from;
+        return Mathf.Atan2(hlp.y, hlp.x);
+    }
+
+    void SetState()
+    {
+        launcher.SetState(Mathf.Abs(sum) / maxSum);
+    }
+
+    void Done()
+    {
+        launcher.SetLaunch();
+    }
+
+}
+
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameObject spoon;
+    [SerializeField]
+    GameObject projectal;          // animal
+    [SerializeField]
+    float speed;                // rotation speed
+    [SerializeField]
+    float lPower;               // launch power an angle  
+    [SerializeField]
+    float lAngle;
+
+    float aFrom;
+    float aTo;
+    float aState;
+
+    float upperBound;           // 270, -90 in editor = after launch
+    float lowerBound;           // 345, -15 in editor = ready to launch
+    float launchingBound;       // around 280/290
+
+    Vector3 launchVector;
+    float lState;
+
+    bool controled;             // is launcher controlled by controller
+    bool working;
+
+
+    void Start()
+    {
+        lowerBound = 345;
+        upperBound = 300;
+        launchingBound = 270;
+        aState = 0;
+        launchVector = new Vector3(Mathf.Cos(Mathf.Deg2Rad*lAngle), Mathf.Sin(Mathf.Deg2Rad * lAngle),0);
+        launchVector = launchVector.normalized;
+        SetAngles(upperBound,lowerBound);
+        controled = true;
+        working = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (working)
+        {
+            spoon.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Lerp(aFrom, aTo, aState)));
+            if (!controled)
+            {
+                StateLogic();
+                if (aState == 1) Launch();
+            }
+        }
+    }
+
+    void StateLogic()
+    {
+        if ((aState + speed) < 1)
+        {
+            aState += speed;
+        }
+        else
+        {
+            aState = 1;
+        }
+    }
+
+    public void SetState(float value)
+    {
+        if(controled)aState = value;
+    }
+
+    public void SetAngles(float from, float to)
+    {
+        aFrom = from;
+        aTo = to;
+    }
+
+    public void SetLaunch()
+    {
+        lState = aState;
+        controled = false;
+        aState = Mathf.Abs((spoon.transform.rotation.eulerAngles.z - lowerBound) / (launchingBound - lowerBound));
+        SetAngles(spoon.transform.rotation.eulerAngles.z, launchingBound);
+    }
+
+    void Launch()
+    {
+        working = false;
+        projectal.transform.parent = null;
+        projectal.GetComponent<Rigidbody>().isKinematic = false;
+        projectal.GetComponent<Rigidbody>().AddForce(launchVector * lPower * lState);
+    }
+}
